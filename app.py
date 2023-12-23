49324485e9d7290334d849c102541eaf5ff8e79e
t0obUnR34L <khocvole1kg3@gmail.com>
2023-12-23 22:25:59 +0700

    init

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/serviceA')
+def service_a():
+    # Xử lý logic cho dịch vụ A
+    # ...
+    return 'Service A'
+
+if __name__ == '__main__':
+    app.run()
