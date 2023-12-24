288af409cd70da8cf5afa2035fe4eaf842426052
t0obUnR34L <khocvole1kg3@gmail.com>
2023-12-24 14:48:55 +0700

    admin+management_service

--- api_gateway/gateway/app.py
+++ api_gateway/gateway/app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 SERVICES = {
     'authentication': 'http://authentication_service:5002/login',
     'authorization': 'http://authorization_service:5005/authorize',
-    'readfile': 'http://readfile_service:5003/readfile',
-    'managefile': 'http://managefile_service:5004/managefile',
+    'admin_service': 'http://admin_service:5003/admin',
+    'management_service': 'http://management_service:5004/management',
 }
 
 
@@ -23,11 +23,9 @@ def gateway_login():
     if request.method == 'GET':
         return jsonify({'message': 'Login endpoint (GET)'})
     elif request.method == 'POST':
-        # Gọi dịch vụ Authentication để xác thực người dùng và chuyển hướng đến Authorization
         auth_response = requests.post(
             SERVICES['authentication'], json=request.get_json())
         if auth_response.status_code == 200:
-            # Xác thực thành công, nhận token từ callback endpoint của Authentication Service
             token_response = auth_response.json()
             token = token_response.get('token')
             return jsonify({'token': token})
@@ -40,61 +38,63 @@ def gateway_register():
     if request.method == 'GET':
         return jsonify({'message': 'Register endpoint (GET)'})
     elif request.method == 'POST':
-        # Logic đăng ký người dùng có thể được thêm vào đây
         return jsonify({'message': 'User registration endpoint'})
 
 
-@app.route('/readfile', methods=['GET', 'POST'])
-def gateway_readfile():
-    if request.method == 'GET':
-        return jsonify({'message': 'Readfile endpoint (GET)'})
-    elif request.method == 'POST':
-        # Gọi dịch vụ Authentication để xác thực người dùng và chuyển hướng đến Authorization
-        auth_response = requests.post(
-            SERVICES['authentication'], json=request.get_json())
-        if auth_response.status_code == 200:
-            # Xác thực thành công, nhận token từ callback endpoint của Authentication Service
-            token_response = auth_response.json()
-            token = token_response.get('token')
+@app.route('/management/read', methods=['GET', 'POST'])
+def gateway_management_read():
+    return manage_operation('read')
 
-            # Kiểm tra vai trò từ token
-            roles = decode_token(token).get('role', '')
 
-            if roles == 'admin' or roles == 'user':
-                # Gọi dịch vụ ReadFile với token
-                readfile_response = requests.post(
-                    SERVICES['readfile'], json={'token': token}).text
-                return jsonify({'readfile_response': readfile_response})
-            else:
-                return jsonify({'error': 'Unauthorized'}), 403
+@app.route('/management/update', methods=['GET', 'POST'])
+def gateway_management_update():
+    return manage_operation('update')
+
+
+@app.route('/management/delete', methods=['GET', 'POST'])
+def gateway_management_delete():
+    return manage_operation('delete')
+
+
+@app.route('/admin', methods=['GET'])
+def gateway_admin():
+    auth_response = requests.post(
+        SERVICES['authentication'], json=request.get_json())
+    if auth_response.status_code == 200:
+        token_response = auth_response.json()
+        token = token_response.get('token')
+
+        roles = decode_token(token).get('role', '')
+
+        if roles == 'admin':
+            # Gọi dịch vụ Admin để xem thông tin database
+            admin_response = requests.get(
+                SERVICES['admin_service'], headers={'Authorization': f'Bearer {token}'}).text
+            return jsonify({'admin_response': admin_response})
         else:
-            return jsonify({'error': 'Authentication failed'}), 401
+            return jsonify({'error': 'Unauthorized'}), 403
+    else:
+        return jsonify({'error': 'Authentication failed'}), 401
 
 
-@app.route('/manage', methods=['GET', 'POST'])
-def gateway_managefile():
+def manage_operation(operation):
     if request.method == 'GET':
-        return jsonify({'message': 'Managefile endpoint (GET)'})
+        return jsonify({'message': f'Management {operation} endpoint (GET)'})
     elif request.method == 'POST':
-        # Gọi dịch vụ Authentication để xác thực người dùng và chuyển hướng đến Authorization
         auth_response = requests.post(
             SERVICES['authentication'], json=request.get_json())
         if auth_response.status_code == 200:
-            # Xác thực thành công, nhận token từ callback endpoint của Authentication Service
             token_response = auth_response.json()
             token = token_response.get('token')
 
-            # Kiểm tra vai trò từ token
             roles = decode_token(token).get('role', '')
 
-            if roles == 'admin':
-                # Gọi dịch vụ ManageFile với token
-                managefile_response = requests.post(
-                    SERVICES['managefile'], json={'token': token}).text
-                return jsonify({'managefile_response': managefile_response})
-            elif roles == 'user':
-                # Nếu là người dùng, từ chối truy cập và chuyển hướng về trang chủ
-                return redirect('/')
+            if roles == 'admin' or (roles == 'user' and operation == 'read'):
+                # Gọi dịch vụ Management với token và thực hiện operation
+                management_response = requests.post(
+                    SERVICES['management_service'],
+                    json={'token': token, 'operation': operation}).text
+                return jsonify({f'management_{operation}_response': management_response})
             else:
                 return jsonify({'error': 'Unauthorized'}), 403
         else:
@@ -102,7 +102,6 @@ def gateway_managefile():
 
 
 def decode_token(token):
-    # Giải mã token và trả về nội dung
     return jwt.decode(token, verify=False)
 
 
