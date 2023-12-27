ea50344b73978e989ee48b269dbf971a0e6fc9be
Truong Long Hung <tlonghung00@gmail.com>
2023-12-27 23:53:20 +0700

    change formatting

--- authentication_service/app.py
+++ authentication_service/app.py
@@ -10,23 +10,28 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 load_dotenv()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("POSTGRESSQL_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("POSTGRESSQL_URI")
 db.init_app(app)
 SECRET_KEY = os.getenv("SECRET_KEY")
-#ALGORITHM = os.getenv("ALGORITHM")
-ISSUER = 'authen-server'
+# ALGORITHM = os.getenv("ALGORITHM")
+ISSUER = "authen-server"
 LIFE_SPAN = 1800
 
 
-@app.route('/api/signup', methods=['POST'])
+@app.route("/api/signup", methods=["POST"])
 def signup():
-    username = request.form.get('username', '')
-    password = request.form.get('password', '')
-    if not (username and password ):
-        return json.dumps({"status":"You have to specify username *and* password"})
+    username = request.form.get("username", "")
+    password = request.form.get("password", "")
+    if not (username and password):
+        return json.dumps({"status": "You have to specify username *and* password"})
 
     if not re.match("^[a-zA-Z0-9_]{1,200}$", username):
-        return json.dumps({"status":"The username should match this regular expression '^[a-zA-Z0-9_]{1,200}$'","status_code":500})
+        return json.dumps(
+            {
+                "status": "The username should match this regular expression '^[a-zA-Z0-9_]{1,200}$'",
+                "status_code": 500,
+            }
+        )
     user = users.query.filter_by(username=username).first()
     try:
         user = users(username, generate_password_hash(password))
@@ -34,24 +39,28 @@ def signup():
         db.session.commit()
     except Exception as e:
         print(e)
-        return json.dumps({"status":"Username already taken"})
-    return json.dumps({"status":"User sign up sucessfully, please return to login"})
+        return json.dumps({"status": "Username already taken"})
+    return json.dumps({"status": "User sign up sucessfully, please return to login"})
 
-@app.route('/api/signin', methods=['POST'])
+
+@app.route("/api/signin", methods=["POST"])
 def signin():
-    username = request.form.get('username', '')
-    password = request.form.get('password', '')
+    username = request.form.get("username", "")
+    password = request.form.get("password", "")
     if not re.match("^[a-zA-Z0-9_]{1,200}$", username):
-        return json.dumps({"status":"The username should match this regular expression '^[a-zA-Z0-9_]{1,200}$'","status_code":500})
+        return json.dumps(
+            {
+                "status": "The username should match this regular expression '^[a-zA-Z0-9_]{1,200}$'",
+                "status_code": 500,
+            }
+        )
     user = users.query.filter_by(username=username).first()
     if not user:
-        return json.dumps({"status":"Invalid username", "status_code":404})
+        return json.dumps({"status": "Invalid username", "status_code": 404})
     if not check_password_hash(user.password, password):
-        return json.dumps({"status":"Invalid password", "status_code":403})
-    return json.dumps({ 
-        "userid":user.userid,
-        "role":user.role
-    })
+        return json.dumps({"status": "Invalid password", "status_code": 403})
+    return json.dumps({"userid": user.userid, "role": user.role})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002)
--- authorization_service/ROPC_auth_server.py
+++ authorization_service/ROPC_auth_server.py
@@ -4,47 +4,54 @@ import time
 
 import jwt
 
-#import ssl
+# import ssl
 from dotenv import load_dotenv
 from flask import Flask, request
 
-ISSUER = 'sample-auth-server'
+ISSUER = "sample-auth-server"
 LIFE_SPAN = 1800
 
-secret_key = 'secret_key'
+secret_key = "secret_key"
 os.getenv("POSTGRESSQL_URI")
 SECRET_KEY = os.getenv("SECRET_KEY")
 app = Flask(__name__)
 
-@app.route('/auth', methods = ['POST'])
+
+@app.route("/auth", methods=["POST"])
 def auth():
-          try:
-              data = request.get_json()
-              userid = data.get('userid')
-              role = data.get('role')
-              if role == 'admin':
-                  scope = 'admin_scope'
-              else:
-                  scope = 'default_scope'
+    try:
+        data = request.get_json()
+        userid = data.get("userid")
+        role = data.get("role")
+        if role == "admin":
+            scope = "admin_scope"
+        else:
+            scope = "default_scope"
 
-              payload = {
-                  "iss": ISSUER,
-                  "exp": time.time() + LIFE_SPAN,
-                  "userid": userid,
-                  "scope": scope
-              }
-              access_token = jwt.encode(payload, secret_key, algorithm = 'HS256')
-              return json.dumps({ 
-                "access_token": access_token,
-                "token_type": "JWT",
-                "expires_in": LIFE_SPAN
-              }),200
-          except Exception as e:
-            return json.dumps({"error": "internal_server_error"}), 500
+        payload = {
+            "iss": ISSUER,
+            "exp": time.time() + LIFE_SPAN,
+            "userid": userid,
+            "scope": scope,
+        }
+
+        access_token = jwt.encode(payload, secret_key, algorithm="HS256")
+        return (
+            json.dumps(
+                {
+                    "access_token": access_token,
+                    "token_type": "JWT",
+                    "expires_in": LIFE_SPAN,
+                }
+            ),
+            200,
+        )
+    except Exception as e:
+        return json.dumps({"error": "internal_server_error"}), 500
 
 
-if __name__ == '__main__':
-  #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-  #context.load_cert_chain('domain.crt', 'domain.key')
-  #app.run(port = 5000, debug = True, ssl_context = context)
-  app.run(port = 5001, debug = True)
+if __name__ == "__main__":
+    # context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    # context.load_cert_chain('domain.crt', 'domain.key')
+    # app.run(port = 5000, debug = True, ssl_context = context)
+    app.run(port=5001, debug=True)
